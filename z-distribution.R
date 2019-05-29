7bc5f3715e6f38d0005c14e032897ffb4f494d91
jwong1MHS <jwong1@ucvts.tec.nj.us>
2019-05-29 05:36:25 -0400

    Made z-distribution graphs

---
+++ z-distribution.R
@@ -0,0 +1,23 @@
+mean <- 5
+popsd <- 2
+n <- 20
+sd <- popsd/sqrt(n)
+error <- qnorm(0.95)*sd
+lb <- a-error
+ub <- a+error
+
+x <- seq(-4,4,length=100)*sd + mean
+hx <- dnorm(x,mean,sd)
+
+plot(x, hx, type="n", xlab="IQ Values", ylab="",
+     main="Normal Distribution", axes=FALSE)
+
+i <- x >= lb & x <= ub
+lines(x, hx)
+polygon(c(lb,x[i],ub), c(0,hx[i],0), col="red") 
+
+area <- pnorm(ub, mean, sd) - pnorm(lb, mean, sd)
+result <- paste("P(",lb,"< IQ <",ub,") =",
+                signif(area, digits=3))
+mtext(result,3)
+axis(1, at=seq(mean-3*sd, mean+3*sd, sd), pos=0)
---
+++ z-score.R
@@ -0,0 +1,6 @@
+#dnorm -> calculating z-score
+#pnorm -> finding area/probability from z-score
+#qnorm -> finding z-score from area
+#rnorm -> random generated normal numbers
+
+dnorm(0, mean = 0, sd = 1)
