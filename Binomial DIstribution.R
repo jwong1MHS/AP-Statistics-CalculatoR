6d286e1e01bd0e08d196dc9b637b758ea3be2fc5
jwong1MHS <jwong1@ucvts.tec.nj.us>
2019-05-29 09:58:13 -0400

    Added Binomial functions

---
+++ Binomial DIstribution.R
@@ -0,0 +1,18 @@
+#binompdf
+dbinom(4, size=12, prob=0.2)
+
+#binomcdf
+pbinom(4, size=12, prob=0.2)
+
+plot(x, hx, type="n", xlab="IQ Values", ylab="",
+     main="Binomial Distribution", axes=FALSE)
+
+i <- x >= lb & x <= ub
+lines(x, hx)
+polygon(c(lb,x[i],ub), c(0,hx[i],0), col="red") 
+
+area <- pnorm(ub, mean, sd) - pnorm(lb, mean, sd)
+result <- paste("P(",lb,"< IQ <",ub,") =",
+                signif(area, digits=3))
+mtext(result,3)
+axis(1, at=seq(mean-3*sd, mean+3*sd, sd), pos=0)
